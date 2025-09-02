f57b2b5747a2317b1e0910632f8afb0a89b185ee
chenusimon <49160235@est.ort.edu.ar>
2025-09-02 11:38:11 -0300

    enemigo movimiento

--- Assets/Scripts/AgentManager.cs
+++ Assets/Scripts/AgentManager.cs
@@ -3,20 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class AgenteCapsulaScript : MonoBehaviour
+public class AgentManager : MonoBehaviour
 {
 
-    public NavMeshAgent agent;
+    [SerializeField] NavMeshAgent agent;
     public Transform targetTR;
+    [SerializeField] Animator anim;
     // Start is called before the first frame update
-    void Awake()
+    void awake()
     {
         agent = GetComponent<NavMeshAgent>();
+        anim = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         agent.destination = targetTR.position;
+        anim.SetFloat("speed", agent.velocity.magnitude);
     }
 }
